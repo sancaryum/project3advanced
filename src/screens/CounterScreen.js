84f5da8a22f1f0d31204ddaf2e538a9098f6d1ca
sancaryum <sancaryum@gmail.com>
2020-11-24 19:23:09 +0300

    useReducer update

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,22 +1,36 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {View,Text,StyleSheet,Button} from 'react-native';
 import { set } from 'react-native-reanimated';
 
+const reducer = (state, action) =>{
+    //state === {count: number}
+    //action ==={type: 'increment || 'decrement', payload:1}
+
+    switch (action.type){
+        case 'increment':
+            return {...state,countumuz: state.countumuz+action.payload}
+        case 'decrement':
+            return{...state,countumuz:state.countumuz-action.payload}
+        default:
+            return state;//butonlardan başka bir şeye tıkladıysa bir şey yapma
+    }
+}
+
 //() yerine (function) da yazılabilir
 const CounterScreen = () =>{
-    const [counter,setCounter] = useState(0);
+    //callMyReducer yerine dispatch yazıyorlar normalde
+   const [state, callMyReducer] = useReducer(reducer,{countumuz:0})
+
 
     return <View>
         <Button
          title="Increase" onPress={()=>{
-            setCounter(counter+1);
-            console.log(counter);
+            callMyReducer({type:'increment', payload:1})
          }}/>
         <Button title="Decrease" onPress={()=>{
-            setCounter(counter-1);
-            console.log(counter);
+            callMyReducer({type:'decrement', payload:1})
         }} />
-        <Text>Sayaç: {counter} </Text>
+        <Text>Sayaç: {state.countumuz} </Text>
 
     </View>
 }
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,49 +1,51 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import { set } from 'react-native-reanimated';
 import ColorCounter from '../components/ColorCounter';
 
 const COLOR_INCREMENT = 15;
 
+const reducer = (state, action) => {
+
+    switch(action.colorToChange){
+        case 'red':
+           return state.red + action.amout > 255 || state.red + action.amount<0
+               ? state //doğruysa bunu
+               :{ ...state, red: state.red + action.amount }; //değilse bunu döndürecek       
+        case 'green':
+            return state.green + action.amout > 255 || state.green + action.amount < 0
+                ? state //doğruysa bunu
+                : { ...state, green: state.green + action.amount }; //değilse bunu döndürecek
+
+        case 'blue':
+            return state.blue + action.amout > 255 || state.blue + action.amount < 0
+                ? state //doğruysa bunu
+                : { ...state, blue: state.blue + action.amount }; //değilse bunu döndürecek
+
+        default:
+            return state;
+    }
+};
+
 const SqueareScreen = () => {
-    const[red,setRed] = useState(0);
-    const[green, setGreen] = useState(0);
-    const[blue, setBlue] = useState(0);
     
-    const setColor=(color, change)=>{
-        //color === 'red', 'green', 'blue'
-        //change === +15, -15
+    const[state,runMyReducer] = useReducer(reducer, {red:0, green:0, blue:0});
+    const{red, green, blue} = state;
 
-       switch(color){
-            case 'red':
-               red + change > 255 || red + change < 0 ? null : setRed(red + change)
-            return;
-            case 'green':   
-               green + change > 255 || green + change < 0 ? null : setGreen(green + change)
-               return;
-            case 'blue':
-                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
-                return;
-            default:
-             //don't do anything
-             return;
-    }}
-    
 
-    console.log(red);
     return (
     <View> 
         <ColorCounter 
-        onIncrease={()=>setColor('red', COLOR_INCREMENT)}
-        onDecrease={()=>setColor('red', -1 * COLOR_INCREMENT)}
+        onIncrease={()=>runMyReducer({colorToChange: 'red', amount: COLOR_INCREMENT})}
+        onDecrease={()=>runMyReducer({colorToChange: 'red', amount: -1 * COLOR_INCREMENT})}
         colorType="red" />
         <ColorCounter 
-        onIncrease={()=>setColor('green', COLOR_INCREMENT)} 
-        onDecrease={()=>setColor('green', -1*COLOR_INCREMENT)}
+        onIncrease={() => runMyReducer({ colorToChange: 'green', amount: COLOR_INCREMENT })} 
+        onDecrease={() => runMyReducer({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })}
         colorType="green" />
         <ColorCounter 
-        onIncrease={()=>setColor('blue', COLOR_INCREMENT)}
-        onDecrease={()=>setColor('blue', -1*COLOR_INCREMENT)}
+        onIncrease={() => runMyReducer({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
+        onDecrease={() => runMyReducer({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })}
         colorType="blue" />
 
         <View 
